e9cc22241c188b52b0b4cff475f32c5269f70037
Caio Cordeiro <ccmr@cesar.org.br>
2021-04-10 16:24:49 -0300

    feat: Add insert and remove operations

--- app/src/main/java/com/example/fruitlist/MainActivity.kt
+++ app/src/main/java/com/example/fruitlist/MainActivity.kt
@@ -2,22 +2,40 @@ package com.example.fruitlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val fruitList = generateDummyList(100)
+    private val adapter = FruitAdapter(fruitList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fruitList = generateDummyList(100)
-
-        recycle_view.adapter = FruitAdapter(fruitList)
+        recycle_view.adapter = adapter
         recycle_view.layoutManager = LinearLayoutManager(this)
         recycle_view.hasFixedSize()
     }
 
-    private fun generateDummyList(size: Int): List<Fruit> {
+    fun insertItem(view: View) {
+        val index = Random.nextInt(8)
+        val newItem = Fruit(
+            R.drawable.ic_baseline_widgets_24, "New item at position $index",
+            "Fruta boa, tem gosto de tamarindo, mas tem cor de limão e parece de laranja"
+        )
+        fruitList.add(index, newItem)
+        adapter.notifyItemInserted(index)
+    }
+
+    fun removeItem(view: View) {
+        val index = Random.nextInt(8)
+        fruitList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+
+    private fun generateDummyList(size: Int): ArrayList<Fruit> {
         val list = ArrayList<Fruit>()
         for (i in 0 until size) {
             val drawable = when (i % 3) {
